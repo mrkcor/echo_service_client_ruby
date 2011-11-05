b422a94b7b0dc5e8d9cfd10084a220c937356021
Mark Kremer <mark@socialreferral.com>
2011-11-05 22:13:28 +0100

    Echo now accepts a parameter in the initialize to determine where output goes.
    Updated tests

--- echo.rb
+++ echo.rb
@@ -11,40 +11,58 @@ end
 HTTPI.log = false
 
 class Echo
-  def initialize
+  def initialize(output = STDOUT)
     # Initialize SOAP client using the WSDL
     @client = Savon::Client.new("http://localhost:9292/echo_service.wsdl")
+    # Set ouput for Echo class
+    @output = output
   end
 
+  # Call Echo on the EchoService
   def echo(message)
-    response = @client.request :echo, "EchoRequest", :body => { "Message" => message } do
-      # The EchoService has its messages in a different namespace than the
-      # targetNamespace of the WSDL, specify it here to have the request
-      # rendered properly
-      soap.namespaces["xmlns:echo"] = "http://www.without-brains.net/echo"
-    end
-    # Get the message from the response and return it
-    data = response.to_array(:echo_response).first
-    data[:message]
+    echo_request(message)
   end
 
+  # Call ReverseEcho on the EchoService
   def reverse_echo(message)
-    response = @client.request :echo, "ReverseEchoRequest", :body => { "Message" => message } do
+    echo_request(message, true)
+  end
+
+  private
+
+  # Send Echo (or ReverseEcho if the reverse parameter is true) request to the EchoService
+  # Return resulting message
+  def echo_request(message, reverse = false)
+    if reverse
+      request_name = "ReverseEcho"
+      response_name = :reverse_echo_response
+    else
+      request_name = "Echo"
+      response_name = :echo_response
+    end
+
+    response = @client.request :echo, "#{request_name}Request", :body => { "Message" => message } do
       # The EchoService has its messages in a different namespace than the
       # targetNamespace of the WSDL, specify it here to have the request
       # rendered properly
       soap.namespaces["xmlns:echo"] = "http://www.without-brains.net/echo"
     end
-    # Get the message from the response and return it
-    data = response.to_array(:reverse_echo_response).first
+    # Get the message from the response and output and return it
+    data = response.to_array(response_name).first
+    @output.puts "EchoService responded to #{request_name}: #{data[:message]}"
     data[:message]
+  rescue Savon::Error => exception
+    # In case of a SOAP fault or HTTPI error output the error message
+    @output.puts "An error occurred while calling #{request_name} on the EchoService: #{exception.message}"
   end
 end
 
 if __FILE__ == $0
   # When running this file directly make use of the Echo class to interact with the EchoService
   message = ARGV[0] || "Hello from Ruby"
+
+  # Initialize the EchoService client and call operations
   echo = Echo.new
-  puts "EchoService responded to Echo: #{echo.echo(message)}"
-  puts "EchoService responded to ReverseEcho: #{echo.reverse_echo(message)}"
+  echo.echo(message)
+  echo.reverse_echo(message)
 end
--- test/test_echo.rb
+++ test/test_echo.rb
@@ -1,17 +1,22 @@
 require_relative 'test_helper'
 
 class TestEcho < MiniTest::Unit::TestCase
+  def setup
+    @output = StringIO.new
+    @echo = Echo.new(@output)
+  end
+
   def test_echo_gives_back_the_echo
-    echo = Echo.new
     VCR.use_cassette("echo") do
-      assert_equal "Hello from Ruby", echo.echo("Hello from Ruby")
+      assert_equal "Hello from Ruby", @echo.echo("Hello from Ruby")
+      assert_equal "EchoService responded to Echo: Hello from Ruby\n", @output.string
     end
   end
 
   def test_reverse_echo_gives_back_the_echo_in_reverse
-    echo = Echo.new
     VCR.use_cassette("reverse_echo") do
-      assert_equal "ybuR morf olleH", echo.reverse_echo("Hello from Ruby")
+      assert_equal "ybuR morf olleH", @echo.reverse_echo("Hello from Ruby")
+      assert_equal "EchoService responded to ReverseEcho: ybuR morf olleH\n", @output.string
     end
   end
 end
